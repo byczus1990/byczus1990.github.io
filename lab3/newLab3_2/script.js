c92093f2958195a22ed86c69ac112cd7da5d8845
szymon.byczek <szymon.byczek@aqg.se>
2022-01-15 15:01:57 +0100

    cleared unused code, added new lab3_2 to github pages

--- lab3/newLab3_2/script.js
+++ lab3/newLab3_2/script.js
@@ -1,12 +1,3 @@
-function drawSquare(){
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
-ctx.fillStyle = randomColor();
-ctx.beginPath();
-ctx.fillRect(20, 20, 20, 20);
-ctx.stroke();
-}
-
 function randomColor(){
 	const randomColor = Math.floor(Math.random()*16777215).toString(16);
 	return "#" + randomColor;
