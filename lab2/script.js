f55167d72f5dc9173ab529a25a4ba1cec96c8110
szymon.byczek <szymon.byczek@aqg.se>
2022-01-20 15:57:03 +0100

    added textArea and drag and drop area for reading css files, added buttons to open file, save file

--- lab2/script.js
+++ lab2/script.js
@@ -1,3 +1,71 @@
+
+
+function onOpenClicked() {
+  if ('FileReader' in window) {
+    document.getElementById('exampleInputFile').click();
+  } else {
+    alert('Your browser does not support the HTML5 FileReader.');
+  }
+};
+
+function onSaveClicked(){
+	if ('Blob' in window) {
+    var fileName = prompt('Please enter file name to save', 'Untitled.css');
+    if (fileName) {
+      var textToWrite = document.getElementById('exampleTextarea').value.replace(/\n/g, '\r\n');
+      var textFileAsBlob = new Blob([textToWrite], { type: 'text/plain' });
+
+      if ('msSaveOrOpenBlob' in navigator) {
+        navigator.msSaveOrOpenBlob(textFileAsBlob, fileName);
+      } else {
+        var downloadLink = document.createElement('a');
+        downloadLink.download = fileName;
+        downloadLink.innerHTML = 'Download File';
+        if ('webkitURL' in window) {
+          // Chrome allows the link to be clicked without actually adding it to the DOM.
+          downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+        } else {
+          // Firefox requires the link to be added to the DOM before it can be clicked.
+          downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+          downloadLink.onclick = destroyClickedElement;
+          downloadLink.style.display = 'none';
+          document.body.appendChild(downloadLink);
+        }
+
+        downloadLink.click();
+      }
+    }
+  } else {
+    alert('Your browser does not support the HTML5 Blob.');
+  }
+};
+
+var holder = document.getElementById('holder'),
+	textField = document.getElementById('exampleTextarea');
+holder.ondragover = function() {
+    this.className = 'hover';
+    return false;
+};
+holder.ondragend = function() {
+    this.className = '';
+    return false;
+};
+holder.ondrop = function(e) {
+    this.className = '';
+    e.preventDefault();
+
+    var file = e.dataTransfer.files[0],
+        reader = new FileReader();
+    reader.onload = function(event) {
+        console.log(event.target);
+        textField.innerText = event.target.result;
+    };
+    console.log(file);
+    reader.readAsText(file);
+
+    return false;
+};
+
 var dragging = null;
 
 document.addEventListener('dragstart', function(event) {
@@ -48,4 +116,55 @@ function getLI( target ) {
     } else {
         return target;
     }
+};
+
+
+document.getElementById('exampleInputFile').onchange = function(event) {
+  var fileToLoad = event.target.files[0];
+
+  if (fileToLoad) {
+    var reader = new FileReader();
+    reader.onload = function(fileLoadedEvent) {
+      var textFromFileLoaded = fileLoadedEvent.target.result;
+      document.getElementById('exampleTextarea').value = textFromFileLoaded;
+    };
+    reader.readAsText(fileToLoad, 'UTF-8');
+  }
+};
+
+document.getElementById('btnSave').onclick = function() {
+  if ('Blob' in window) {
+    var fileName = prompt('Please enter file name to save', 'Untitled.txt');
+    if (fileName) {
+      var textToWrite = document.getElementById('exampleTextarea').value.replace(/\n/g, '\r\n');
+      var textFileAsBlob = new Blob([textToWrite], { type: 'text/plain' });
+
+      if ('msSaveOrOpenBlob' in navigator) {
+        navigator.msSaveOrOpenBlob(textFileAsBlob, fileName);
+      } else {
+        var downloadLink = document.createElement('a');
+        downloadLink.download = fileName;
+        downloadLink.innerHTML = 'Download File';
+        if ('webkitURL' in window) {
+          // Chrome allows the link to be clicked without actually adding it to the DOM.
+          downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+        } else {
+          // Firefox requires the link to be added to the DOM before it can be clicked.
+          downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+          downloadLink.onclick = destroyClickedElement;
+          downloadLink.style.display = 'none';
+          document.body.appendChild(downloadLink);
+        }
+
+        downloadLink.click();
+      }
+    }
+  } else {
+    alert('Your browser does not support the HTML5 Blob.');
+  }
+};
+
+function destroyClickedElement(event) {
+  document.body.removeChild(event.target);
 }
+
